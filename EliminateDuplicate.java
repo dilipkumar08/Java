8bd649663535de306aea16bcefbff2a95653db23
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2023-03-13 11:56:38 +0530

    Update EliminateDuplicate.java

--- EliminateDuplicate.java
+++ EliminateDuplicate.java
@@ -4,13 +4,14 @@ import java.lang.*;
 public class Main {
     static int [] EliminateDuplicate(int[] arr)
     {
-        int l=arr.length-1;
+        int l=arr.length;
+        int o=0;
         List<Integer> res = new ArrayList<>();
         int a=0;
-        for(int i=0;i<l;i++) {
+        for(int i=0;i<l-1;i++) {
             for (int j = i + 1; j < l; j++) {
                 if (arr[i] > arr[j]) {
-                    System.out.println(arr[i] + " " + arr[j]);
+
                     a = arr[j];
                     arr[j] = arr[i];
                     arr[i] = a;
@@ -22,20 +23,20 @@ public class Main {
             if(res.size()==0)
             {
                 res.add(arr[u]);
+                o+=1;
             }
             else
             {
-                if (res.get(u-1)!=arr[u]);
+                if (res.get(o-1)!=arr[u])
                 {
+                    o+=1;
                     res.add(arr[u]);
                 }
             }
         }
-        System.out.println(Arrays.toString(arr));
         int []resArr=new int[res.size()];
-        for (int k=0;k<res.size();k++)
-        {
-            resArr[k]=res.get(k);
+        for (int k=0;k<res.size();k++) {
+            resArr[k] = res.get(k);
         }
         return resArr;
     }
@@ -54,14 +55,26 @@ public class Main {
             int size = sc.nextInt();
             int arr[] = new int[size];
             System.out.println("Enter the elements for the array: ");
-            for (int k = 0; k < size; k++) {
+            for (int k = 0; k < size; k++)
+            {
                 arr[k] = sc.nextInt();
             }
             int[] res = EliminateDuplicate(arr);
-            for (int j = 0; j < res.length; j++) {
+            System.out.println(Arrays.toString(res));
+            System.out.println("Bill ID numbers:"+Arrays.toString(arr));
+            System.out.println("Bill ID Distinct:"+Arrays.toString(res));
+            for (int j = 0; j < res.length; j++)
+            {
                 stmt.executeUpdate("INSERT INTO billing VALUES(" + (j + 1) + "," + res[j] + ")");
             }
-            ResultSet rs=stmt.executeQuery("SELECT ID FROM BILLING ");
+            ResultSet rs=stmt.executeQuery("SELECT* FROM BILLING ");
+            while(rs.next())
+            {
+                int sno=rs.getInt("SNO");
+                int ids=rs.getInt("ID");
+                System.out.println("SNO:"+sno+"    "+"ID:"+ids);
+
+            }
             rs.close();
             stmt.close();
         }

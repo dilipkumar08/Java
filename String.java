711e9d65b3f58fcadd6d6b4788c6157d818d4798
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2023-12-05 00:52:03 +0530

    Create String.java
    just a simple java program to show the different methods of string

---
+++ String.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String st="Daijoubu";
+        System.out.println("Dilip kumar");
+        System.out.println(st.length());
+        System.out.println(st.toUpperCase());
+        System.out.println(st.toLowerCase());
+        System.out.println(st.indexOf("D"));
+    }
+    
+}
